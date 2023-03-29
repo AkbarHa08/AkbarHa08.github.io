c2fe4dee71db4253e904883b266849df88e472f5
AkbarHa08 <68279197+AkbarHa08@users.noreply.github.com>
2023-03-29 23:10:59 +0400

    change theme yazildi

--- live-clock/js/index.js
+++ live-clock/js/index.js
@@ -3,7 +3,12 @@ var clockField = document.getElementById('clock-field');
 var infoText = document.getElementById('info-text');
 var clockText = document.getElementById('clock-text');
 var dateText = document.getElementById('date-text');
-var clockArea = document.getElementById('clock-area');
+var clockArea = document.getElementById('clock-area-id');
+var navbar = document.getElementById('navbar-id');
+var navbarTexts = document.getElementsByClassName('text-color');
+var actionsText = document.getElementById('actions-text');
+
+var darkTheme = true;
 
 var date = new Date;
 
@@ -39,8 +44,41 @@ setInterval(setTime,1000);
 
 
 function changeTheme(){
-    clockArea.classList.remove("bg-dark");
-    clockArea.classList.add("bg-light");
+    if( darkTheme ){
+        darkTheme = false;
+        clockArea.classList.remove("bg-dark");
+        clockArea.classList.add("bg-light");
+        navbar.classList.remove("bg-light");
+        navbar.classList.add("bg-dark");
+        for( let i = 0; i < 3; i++ ){
+            navbarTexts[i].style.color = "white";
+        }
+        infoText.classList.remove("text-light");
+        infoText.classList.add("text-dark");
+        clockText.classList.remove("text-light");
+        clockText.classList.add("text-dark");
+        dateText.classList.remove("text-light");
+        dateText.classList.add("text-dark");
+        actionsText.classList.remove("text-light");
+        actionsText.classList.add("text-dark");
+    } else{
+        darkTheme = true;
+        clockArea.classList.remove("bg-light");
+        clockArea.classList.add("bg-dark");
+        navbar.classList.remove("bg-dark");
+        navbar.classList.add("bg-light");
+        for( let i = 0; i < 3; i++ ){
+            navbarTexts[i].style.color = "black";
+        }
+        infoText.classList.remove("text-dark");
+        infoText.classList.add("text-light");
+        clockText.classList.remove("text-dark");
+        clockText.classList.add("text-light");
+        dateText.classList.remove("text-dark");
+        dateText.classList.add("text-light");
+        actionsText.classList.remove("text-dark");
+        actionsText.classList.add("text-light");
+    }
 }
 
 
